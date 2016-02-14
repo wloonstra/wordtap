16acbee9e36cdb2c84019117a241e0d5b6175e5d
Wiggert <wiggertloonstra@gmail.com>
2016-02-14 12:50:32 +0100

    orientation landscape, proper colors

--- app/src/main/java/nl/wiggertloonstra/wordtap/WordTap.java
+++ app/src/main/java/nl/wiggertloonstra/wordtap/WordTap.java
@@ -1,13 +1,8 @@
 package nl.wiggertloonstra.wordtap;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import nl.wiggertloonstra.wordtap.library.WordLibrary;
@@ -19,10 +14,17 @@ public class WordTap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_tap);
 
-        setupTappingOnWordContainer();
+        styleTextSize();
+        setupClickToNextWord();
     }
 
-    private void setupTappingOnWordContainer() {
+    private void styleTextSize() {
+        View globalContainer = findViewById(R.id.global_container);
+        TextView wordContainer = (TextView) findViewById(R.id.word_container);
+        wordContainer.setTextSize(60);
+    }
+
+    private void setupClickToNextWord() {
         View globalContainer = findViewById(R.id.global_container);
         globalContainer.setOnClickListener(new View.OnClickListener() {
             @Override
