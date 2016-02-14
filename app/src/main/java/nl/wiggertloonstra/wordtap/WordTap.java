c7fd74aba3568414d0dddd9b85c0a167f8a5e3ff
Wiggert <wiggertloonstra@gmail.com>
2016-02-14 12:18:10 +0100

    tap on screen and see next word

--- app/src/main/java/nl/wiggertloonstra/wordtap/WordTap.java
+++ app/src/main/java/nl/wiggertloonstra/wordtap/WordTap.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import nl.wiggertloonstra.wordtap.library.WordLibrary;
 
 public class WordTap extends AppCompatActivity {
 
@@ -15,38 +18,22 @@ public class WordTap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_tap);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setupTappingOnWordContainer();
+    }
+
+    private void setupTappingOnWordContainer() {
+        View globalContainer = findViewById(R.id.global_container);
+        globalContainer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                displayNextWord();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_word_tap, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void displayNextWord() {
+        TextView wordContainer = (TextView) findViewById(R.id.word_container);
+        wordContainer.setText(WordLibrary.getInstance().getNextWord());
     }
 }
---
+++ app/src/main/java/nl/wiggertloonstra/wordtap/library/WordLibrary.java
@@ -0,0 +1,34 @@
+package nl.wiggertloonstra.wordtap.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordLibrary {
+
+    private static final List<String> words = new ArrayList<>();
+    private static int index = 0;
+
+    private static WordLibrary instance;
+
+    private WordLibrary() {
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+    }
+
+    public static WordLibrary getInstance() {
+        if (instance == null) {
+            instance = new WordLibrary();
+        }
+        return instance;
+    }
+
+    public String getNextWord() {
+        String newWord = words.get(index);
+        index = (index + 1) % words.size();
+        return newWord;
+    }
+
+}
